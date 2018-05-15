a474752c5edbdf8697ecf3e703a9b9ce9b7fbbfb
Maiko <maikossmaster@gmail.com>
2018-05-15 08:41:17 -0300

    Init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React, {Component, Fragment} from 'react';
+import {render} from 'react-dom';
+
+class Button extends Component{
+    render(){
+        return <a href="" onClick={this.props.onClick}>
+                        {this.props.children}
+                Enviar</a>;
+    }
+}
+
+class App extends Component{
+    
+    handleClick(){
+        alert("Botão clicado!");
+    }
+
+    render(){
+        return(
+            <Fragment>
+                <h1>Hello React</h1>
+                <Button onClick={this.handleClick}>Enviar</Button>
+            </Fragment>
+        );
+    }
+}
+
+render (<App />, document.getElementById("app"));
